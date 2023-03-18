fa18c4380402ddd97b7bbb37f38491e5ef03ea06
Thanakorn Sriwannawit <64070040@kmitl.ac.th>
2023-03-18 21:44:17 +0700

    install gsap

--- src/App.vue
+++ src/App.vue
@@ -14,9 +14,11 @@ import HelloWorld from "./components/HelloWorld.vue";
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
       </nav>
+      <div class="bg-red-500 fadeSlideTo">to</div>
+      <div class="bg-green-500 fadeSlideFrom">from</div>
+      <div class="bg-blue-500 fadeSlideFromTo">fromTo</div>
     </div>
   </header>
 
   <RouterView />
 </template>
-
--- src/main.js
+++ src/main.js
@@ -5,6 +5,8 @@ import App from './App.vue'
 import router from './router'
 
 import './assets/main.css'
+import '/src/gsap.js'
+
 
 const app = createApp(App)
 
