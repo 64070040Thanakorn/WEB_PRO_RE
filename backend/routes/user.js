2a81342680472b8b5d13be224432e20b5d90173d
7E8J <103146285+7E8J@users.noreply.github.com>
2023-04-04 21:30:27 +0700

    [update] backend authentication
    เพิ่ม register
    เพิ่ม delete account

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,18 +1,54 @@
+const { PrismaClient } = require("@prisma/client")
 const express = require("express")
+const prisma = new PrismaClient()
 
 router = express.Router();
 
-const user =  {
-  id: 10,
-  first_name: "Norrapat",
-  last_name: "Srimoonoi",
-  email: "example@gmail.com",
-  password: "123456789",
-  tel: "0123456789"
-}
+router.get("/api/user" , async (req,res,next) => {
+  try {
+    const user = await prisma.user.findMany();
+    res.json(user);
+  } catch(err) {
+    next(err);
+  };
+});
 
-router.get("/api/user" , async function(req,res,next){
-  res.send(JSON.stringify(user))
-})
+router.post("/register", async (req,res,next) => {
+  try{
+    const { firstName, lastName, email, password } = req.body;
+    const user = await prisma.user.create({
+      data:{
+        first_name: firstName,
+        last_name: lastName,
+        email: email,
+        password: password,
+      }
+    });
+    res.json(user)
+  } catch(err) {
+    next(err);
+  };
+});
+
+// router.post("/login", async(req, res, next) => {
+//   try{
+
+//   } catch(err) {
+//     next(err);
+//   };
+// });
+
+router.delete("/:id/deleteAcc", async(req, res, next) => {
+  try{
+    const user = await prisma.user.delete({
+      where:{
+        id: parseInt(req.params.id)
+      }
+    })
+    res.send(`delete account id ${req.params.id}`)
+  } catch(err){
+    next(err)
+  }
+});
 
 exports.router = router
